3fcfe7f101ba8174a6a08cfbdcc664f26363ccde
ninjudd <git@justinbalthrop.com>
2011-03-10 17:18:40 -0800

    initial code from jiraph

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
 (defproject ruminate "0.0.1-SNAPSHOT"
   :description "Key value data store for Clojure with pluggable backends."
-  :dependencies [[clojure "1.2.0"]])
+  :dependencies [[clojure "1.2.0"]
+                 [tokyocabinet "1.23-SNAPSHOT"]
+                 [clojure-useful "0.3.1"]
+                 [retro "0.5.0-SNAPSHOT"]])
---
+++ src/ruminate/db.clj
@@ -0,0 +1,17 @@
+(ns ruminate.db
+  (:refer-clojure :exclude [get]))
+
+(defprotocol DB "Key-value database."
+  (open       [db])
+  (close      [db])
+  (sync!      [db])
+  (optimize!  [db])
+  (get        [db key])
+  (len        [db key])
+  (key-seq    [db])
+  (add!       [db key val])
+  (put!       [db key val])
+  (append!    [db key val])
+  (inc!       [db key i])
+  (delete!    [db key])
+  (truncate!  [db]))
