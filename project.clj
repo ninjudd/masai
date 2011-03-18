f913f26284ba4e171acad6c98281362c601f0e1d
ninjudd <git@justinbalthrop.com>
2011-03-18 12:21:18 -0700

    version 0.5.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ruminate "0.5.0-SNAPSHOT"
+(defproject ruminate "0.5.0"
   :description "Key-value database for Clojure with pluggable backends."
   :dependencies [[clojure "1.2.0"]
-                 [tokyocabinet "1.23-SNAPSHOT"]
-                 [clojure-useful "0.3.1"]
-                 [retro "0.5.0-SNAPSHOT"]])
+                 [tokyocabinet "1.24.0"]
+                 [clojure-useful "0.3.2"]
+                 [retro "0.5.0"]])
