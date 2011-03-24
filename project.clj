19f5222ccbf45f5c9ef85c920339c53759597958
ninjudd <git@justinbalthrop.com>
2011-03-24 13:58:17 -0700

    update useful

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Key-value database for Clojure with pluggable backends."
   :dependencies [[clojure "1.2.0"]
                  [tokyocabinet "1.24.1-SNAPSHOT"]
-                 [clojure-useful "0.3.2"]
+                 [clojure-useful "0.3.3"]
                  [retro "0.5.0"]])
