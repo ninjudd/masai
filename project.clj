dcd47439afaec56bce7e2d5acfbe7003fb897460
Raynes <i@raynes.me>
2011-04-25 18:17:52 -0500

    Added spymemcached to the deps.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject masai "0.5.1-SNAPSHOT"
   :description "Key-value database for Clojure with pluggable backends."
+  :repositories {"spymemcached" "http://bleu.west.spy.net/~dustin/m2repo/"}
   :dependencies [[clojure "1.2.0"]
                  [tokyocabinet "1.24.1-SNAPSHOT"]
                  [clojure-useful "0.3.3"]
-                 [retro "0.5.0"]])
+                 [retro "0.5.0"]
+                 [spy/memcached "2.4rc1"]])
