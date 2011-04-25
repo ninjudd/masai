b69c186f237c7bbd1ec723f789a0eb205f855d23
Raynes <i@raynes.me>
2011-04-25 18:18:21 -0500

    Added memcached backend.

---
+++ src/masai/memcached.clj
@@ -0,0 +1,30 @@
+(ns masai.memcached
+  (:require masai.db)
+  (:import net.spy.memcached.MemcachedClient
+           [java.net InetSocketAddress InetAddress]))
+
+(deftype DB [#^MemcachedClient mdb key-format]
+  masai.db/EphemeralDB
+  
+  (add-expiry! [db key val exp] (.add mdb (key-format key) exp val))
+  (put-expiry! [db key val exp] (.set mdb (key-format key) exp val))
+  
+  masai.db/DB
+  
+  (close [db] (.shutdown mdb))
+  (get [db key] (.get mdb (key-format key)))
+  (add! [db key val] (masai.db/add-expiry! db key val 0))
+  (put! [db key val] (masai.db/put-expiry! db key val 0))
+  (append!
+   [db key val]
+   (let [fkey (key-format key)]
+     (.append mdb (.getCas (.gets mdb fkey)) fkey val)))
+  (inc! [db key i] (.incr mdb (key-format key) i))
+  (delete! [db key] (.delete mdb (key-format key))))
+
+(defn make [key-format & addresses]
+  (DB.
+   (MemcachedClient.
+    (for [[addr port] addresses]
+      (InetSocketAddress. (InetAddress/getByName addr) port)))
+   key-format))
