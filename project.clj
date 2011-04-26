7c0993c5d1f14a25ab324a5d0c6bbd248c5830a2
Raynes <i@raynes.me>
2011-04-26 14:50:11 -0500

    Added jedis to dependencies.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                  [tokyocabinet "1.24.1-SNAPSHOT"]
                  [clojure-useful "0.3.3"]
                  [retro "0.5.0"]
-                 [spy/memcached "2.4rc1"]])
+                 [spy/memcached "2.4rc1"]
+                 [redis.clients/jedis "1.5.2"]])
