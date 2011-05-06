5c418a367a8116105672034f9b8263618284d579
Anthony Grimes <i@raynes.me>
2011-05-05 21:35:51 -0500

    Get rid of added spymemcached repository since I've uploaded it to clojars.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject masai "0.5.1-SNAPSHOT"
   :description "Key-value database for Clojure with pluggable backends."
-  :repositories {"spymemcached" "http://bleu.west.spy.net/~dustin/m2repo/"}
   :dependencies [[clojure "1.2.0"]
                  [tokyocabinet "1.24.1-SNAPSHOT"]
                  [clojure-useful "0.3.3"]
