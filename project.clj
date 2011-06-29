b4ccfaf8a926ddac8d5e45801157d48afd2ba8aa
Justin Balthrop <git@justinbalthrop.com>
2011-06-29 16:59:12 -0700

    snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject masai "0.5.2"
+(defproject masai "0.5.2-SNAPSHOT"
   :description "Key-value database for Clojure with pluggable backends."
   :dependencies [[clojure "1.2.0"]
                  [useful "0.4.0"]
