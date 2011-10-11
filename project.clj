f3a0f6324453f02d63c7e3694453a26487ea0b97
Justin Balthrop <git@justinbalthrop.com>
2011-10-11 13:29:01 -0700

    0.6.0

--- project.clj
+++ project.clj
@@ -1,11 +1,8 @@
-(defproject masai "0.6.0-alpha1"
+(defproject masai "0.6.0"
   :description "Key-value database for Clojure with pluggable backends."
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.0-beta4"]
-                 [retro "0.5.0"]]
-  :dev-dependencies [[tokyocabinet "1.24.1-SNAPSHOT"]
+                 [useful "0.7.0"]
+                 [retro "0.5.2"]]
+  :dev-dependencies [[tokyocabinet "1.24.1-SNAPSHOT" :ext true]
                      [spy/memcached "2.4rc1"]
-                     [org.clojars.raynes/jedis "2.0.0-SNAPSHOT"]
-                     [clojure-contrib "1.2.0"]
-                     [org.clojars.flatland/cake-marginalia "0.6.3"]]
-  :tasks [cake-marginalia.tasks])
+                     [org.clojars.raynes/jedis "2.0.0-SNAPSHOT"]])
