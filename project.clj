4ffdb6331936685435fe5321afb63c6837c00826
Anthony Grimes <i@raynes.me>
2011-12-02 15:50:03 -0600

    Update to 1.3.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject masai "0.7.0-alpha4"
   :description "Key-value database for Clojure with pluggable backends."
-  :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.1"]
+  :dependencies [[clojure "1.3.0"]
+                 [useful "0.7.5-alpha3"]
                  [retro "0.6.0-alpha3"]]
-  :dev-dependencies [[tokyocabinet "1.24.1-SNAPSHOT" :ext true]
+  :dev-dependencies [[tokyocabinet "1.24.1-SNAPSHOT"]
                      [spy/memcached "2.4rc1"]
                      [org.clojars.raynes/jedis "2.0.0-SNAPSHOT"]])
