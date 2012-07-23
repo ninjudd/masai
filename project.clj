eb8263d42f26a18234562525586cf44820d9cfd2
Anthony Grimes <i@raynes.me>
2012-07-23 18:09:41 -0500

    Update project.clj to lein2, update deps, and test against multiple Clojure versions.

--- project.clj
+++ project.clj
@@ -1,8 +1,17 @@
 (defproject masai "0.7.0-alpha8"
+  :url "https://github.com/flatland/masai"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :description "Key-value database for Clojure with pluggable backends."
-  :dependencies [[clojure "1.3.0"]
-                 [useful "0.7.5-alpha3"]
-                 [retro "0.6.0-alpha3"]]
-  :dev-dependencies [[tokyocabinet "1.24.2"]
-                     [spy/memcached "2.4rc1"]
-                     [org.clojars.raynes/jedis "2.0.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [useful "0.8.3-alpha8"]
+                 [retro "0.6.1-alpha1"]]
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :dev {:dependencies [[tokyocabinet "1.24.4"]
+                                  [spy/memcached "2.4rc1"]
+                                  [org.clojars.raynes/jedis "2.0.0-SNAPSHOT"]]}}
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.3,default:dev,1.5,default" "test"]}
+  :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}})
